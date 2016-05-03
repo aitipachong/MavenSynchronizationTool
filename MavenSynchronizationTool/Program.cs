000f41c04addc29b115160163f2fe53685adf2f3
aitipachong <laiqiang0010@163.com>
2016-05-03 17:22:34 +0800

    增加APP初始化，从SQLite库中获取同步文件后缀类型。

--- MavenSynchronizationTool/Program.cs
+++ MavenSynchronizationTool/Program.cs
@@ -12,6 +12,7 @@ namespace MavenSynchronizationTool
 {
     public static class Program
     {
+        private const string SQLITE_NAME = "MavenSynchDb.db";           //SQLite数据库文件名称
         private const string IS_HTTP_PROXY_KEY = "IsHttpProxy";
         private const string HTTP_PROXY_HOST_KEY = "HttpProxyHost";
         private const string HTTP_PROXY_PORT_KEY = "HttpProxyPort";
@@ -19,6 +20,16 @@ namespace MavenSynchronizationTool
         private const string HTTP_PROXY_PWD_KEY = "HttpProxyPwd";
         private const string DOWNLOAD_TYPE_KEY = "DownloadType";
         private const string MAPPING_CONFIG_KEY = "MappingConfig";
+
+        /// <summary>
+        /// SQLite数据库链接字符串
+        /// </summary>
+        public static string SQLiteConnectionString { get; set; }
+
+        /// <summary>
+        /// 从SQLite数据库DownloadSuffix表提取的同步文件后缀列表
+        /// </summary>
+        public static List<string> SuffixList { get; set; } = new List<string>();
 
         public static bool IsHttpProxy { get; set; }
         public static string HttpProxyHost { get; set; }
@@ -37,31 +48,46 @@ namespace MavenSynchronizationTool
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            UT_ExecuteDataSet();
+            //获取Maven同步的文件后缀类型列表
+            if(!GetMavenSynchFileSuffix())
+            {
+                Application.Exit();
+            }
 
             GetSystemHttpProxyConfiguration();
 
             Application.Run(new Form1());
         }
 
-
-        public static void UT_ExecuteDataSet()
+        /// <summary>
+        /// 获取Maven中央库同步内容后缀
+        /// </summary>
+        private static bool GetMavenSynchFileSuffix()
         {
-            string sqliteConnectionString = "Data Source={0};Version=3;";
-            string sqlitePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MavenSynchDb.db");
-            if (!File.Exists(sqlitePath))
+            string tempConnectionStr = "Data Source={0};Version=3;";
+            string sqlitePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, SQLITE_NAME);
+            if(!File.Exists(sqlitePath))
             {
-                throw new ArgumentException("SQLite file is not exists.");
+                MessageBox.Show(string.Format("SQLite数据库文件不存在，路径：{0}", sqlitePath), "异常", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
             }
-            string connectionStr = string.Format(sqliteConnectionString, sqlitePath);
-            string commandText = "SELECT id, suffix, ignore FROM DownloadSuffix WHERE ignore = 0";
 
-            DataSet ds = SQLiteHelper.ExecuteDataSet(connectionStr, commandText, null);
+            SQLiteConnectionString = string.Format(tempConnectionStr, sqlitePath);
+            string commandText = "SELECT suffix FROM DownloadSuffix WHERE ignore = 0";
+            DataSet ds = SQLiteHelper.ExecuteDataSet(SQLiteConnectionString, commandText, null);
             if (ds == null || ds.Tables == null || ds.Tables.Count == 0 || ds.Tables[0].Rows == null || ds.Tables[0].Rows.Count == 0)
             {
-                throw new ArgumentException("ExecuteDataSet fail.");
+                MessageBox.Show("SQLite数据库DownloadSuffix表没有内容.", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
             }
 
+            if (SuffixList == null) SuffixList = new List<string>();
+            for(int loopi = 0;loopi < ds.Tables[0].Rows.Count;loopi++)
+            {
+                SuffixList.Add(ds.Tables[0].Rows[loopi]["suffix"].ToString());
+            }
+
+            return true;
         }
 
         private static void GetSystemHttpProxyConfiguration()
