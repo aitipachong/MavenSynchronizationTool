51d6d8c39cc81ea4e589c49c00eb254aeb75c64f
aitipachong <laiqiang0010@163.com>
2016-05-10 16:15:12 +0800

    修改主窗体，且重新定义窗体名称。

--- MavenSynchronizationTool/FrmMain.Designer.cs
+++ MavenSynchronizationTool/FrmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MavenSynchronizationTool
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 394);
+            this.Name = "FrmMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
