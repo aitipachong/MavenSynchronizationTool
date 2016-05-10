99233828a533b1791435b7776416db532221246e
aitipachong <laiqiang0010@163.com>
2016-05-10 16:12:14 +0800

    更新Program.cs文件，把系统初始化内容进行独立。

--- MavenSynchronizationTool/Program.cs
+++ MavenSynchronizationTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MavenSynchronizationTool.Core.DBUtility;
+using MavenSynchronizationTool.Entities;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -12,14 +13,6 @@ namespace MavenSynchronizationTool
 {
     public static class Program
     {
-        private const string SQLITE_NAME = "MavenSynchDb.db";           //SQLite数据库文件名称
-        private const string IS_HTTP_PROXY_KEY = "IsHttpProxy";
-        private const string HTTP_PROXY_HOST_KEY = "HttpProxyHost";
-        private const string HTTP_PROXY_PORT_KEY = "HttpProxyPort";
-        private const string HTTP_PROXY_USER_KEY = "HttpProxyUser";
-        private const string HTTP_PROXY_PWD_KEY = "HttpProxyPwd";
-        private const string DOWNLOAD_TYPE_KEY = "DownloadType";
-        private const string MAPPING_CONFIG_KEY = "MappingConfig";
 
         /// <summary>
         /// SQLite数据库链接字符串
@@ -31,13 +24,21 @@ namespace MavenSynchronizationTool
         /// </summary>
         public static List<string> SuffixList { get; set; } = new List<string>();
 
-        public static bool IsHttpProxy { get; set; }
-        public static string HttpProxyHost { get; set; }
-        public static int HttpProxyPort { get; set; }
-        public static string HttpProxyUser { get; set; }
-        public static string HttpProxyPwd { get; set; }
-        public static List<string> DownloadTypes { get; set; }
-        public static string ConfigFilePath { get; set; }
+        public static bool IsHttpProxy { get; set; }            //是否使用HTTP代理
+        public static string HttpProxyHost { get; set; }        //HTTP代理的Host
+        public static int HttpProxyPort { get; set; }           //HTTP代理的Port
+        public static string HttpProxyUser { get; set; }        //HTTP代理的用户名
+        public static string HttpProxyPwd { get; set; }         //HTTP代理的密码
+
+        /// <summary>
+        /// 检索Maven中央库的文件名列表
+        /// </summary>
+        public static List<string> RetrievalFileNames { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Maven中央库信息列表
+        /// </summary>
+        public static List<CentralRepository> CentralRepositoryList { get; set; } = new List<CentralRepository>();
 
         /// <summary>
         /// 应用程序的主入口点。
@@ -47,77 +48,7 @@ namespace MavenSynchronizationTool
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            //获取Maven同步的文件后缀类型列表
-            if(!GetMavenSynchFileSuffix())
-            {
-                Application.Exit();
-            }
-
-            GetSystemHttpProxyConfiguration();
-
-            Application.Run(new Form1());
-        }
-
-        /// <summary>
-        /// 获取Maven中央库同步内容后缀
-        /// </summary>
-        private static bool GetMavenSynchFileSuffix()
-        {
-            string tempConnectionStr = "Data Source={0};Version=3;";
-            string sqlitePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, SQLITE_NAME);
-            if(!File.Exists(sqlitePath))
-            {
-                MessageBox.Show(string.Format("SQLite数据库文件不存在，路径：{0}", sqlitePath), "异常", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
-            }
-
-            SQLiteConnectionString = string.Format(tempConnectionStr, sqlitePath);
-            string commandText = "SELECT suffix FROM DownloadSuffix WHERE ignore = 0";
-            DataSet ds = SQLiteHelper.ExecuteDataSet(SQLiteConnectionString, commandText, null);
-            if (ds == null || ds.Tables == null || ds.Tables.Count == 0 || ds.Tables[0].Rows == null || ds.Tables[0].Rows.Count == 0)
-            {
-                MessageBox.Show("SQLite数据库DownloadSuffix表没有内容.", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
-            }
-
-            if (SuffixList == null) SuffixList = new List<string>();
-            for(int loopi = 0;loopi < ds.Tables[0].Rows.Count;loopi++)
-            {
-                SuffixList.Add(ds.Tables[0].Rows[loopi]["suffix"].ToString());
-            }
-
-            return true;
-        }
-
-        private static void GetSystemHttpProxyConfiguration()
-        {
-            try
-            {
-                IsHttpProxy = Convert.ToBoolean(ConfigurationManager.AppSettings[IS_HTTP_PROXY_KEY].ToString());
-                HttpProxyHost = ConfigurationManager.AppSettings[HTTP_PROXY_HOST_KEY].ToString();
-                HttpProxyPort = Convert.ToInt32(ConfigurationManager.AppSettings[HTTP_PROXY_PORT_KEY].ToString());
-                HttpProxyUser = ConfigurationManager.AppSettings[HTTP_PROXY_USER_KEY].ToString();
-                HttpProxyPwd = ConfigurationManager.AppSettings[HTTP_PROXY_PWD_KEY].ToString();
-
-                string strDownloadType = ConfigurationManager.AppSettings[DOWNLOAD_TYPE_KEY].ToString();
-                if (!string.IsNullOrEmpty(strDownloadType))
-                {
-                    DownloadTypes = new List<string>();
-                    DownloadTypes = strDownloadType.Split(';').ToList<string>();
-                }
-
-                string relativePath = ConfigurationManager.AppSettings[MAPPING_CONFIG_KEY].ToString();
-                if(!string.IsNullOrEmpty(relativePath))
-                {
-                    ConfigFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
-                }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message, "异常", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.Exit();
-            }
+            Application.Run(new FrmSysInit());
         }
     }
 }
